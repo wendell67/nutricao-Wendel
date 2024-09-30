8d97679eafee319aa63df1e4a0c10bc370660f6e
Wendel Vinicius <133383757+wendell67@users.noreply.github.com>
2024-09-30 10:39:27 -0300

    30/09/24

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+var título = document.querySelector(".título");
+título.textContent = "Wendel Nutrição";
 
-
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Wendel Nutrição";
-
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+var pacientes = document.querySelector("#paciente");
+for (var i = 0; i < 5; i++) {
+  var tdPeso = paciente[i].querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
+var tdAltura = paciente[i].querySelector(".info-altura");
 var altura = tdAltura.textContent;
+
+var tdImc = pacientes[i].querySelector(".info-imc");
+
+var pesoVal = true;
+var alturaVal =true;
+
+if (peso < 0 || peso > 600) {
+
+  tdImc.textContent = "peso inválido";
+  pesoVal = false;
+  pacientes [i].style.color = "orange";
+
+}
+
+if (altura < 0 || altura > 2.80) {
+
+  tdImc.textContent = "altura não existente"
+  alturaVal =false;
+}
+
+if (pesoVal && alturaVal) {
+
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
+}
